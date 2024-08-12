6623dfce8d0815496a5c29f7288494bab766beef
Alex Haszard <aggis.haszard@gmail.com>
2024-08-12 12:06:51 +1200

    add container for backend API

--- docker-entrypoint-initdb.d/db.sql
+++ docker-entrypoint-initdb.d/db.sql
@@ -3,3 +3,7 @@ create table Jobs (
     Job_Num int primary key,
     Issue varchar(255)
 );
+
+insert into Jobs (Job_Num, Issue) values (1, "Pothole");
+insert into Jobs (Job_Num, Issue) values (2, "Fallen Tree");
+insert into Jobs (Job_Num, Issue) values (3, "Line Painting");
